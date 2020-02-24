47f03c4d4040b40c46f7d8194096b287b4015232
Housse-code <61385942+Housse-code@users.noreply.github.com>
2020-02-24 18:35:27 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,11 @@
-//set the value of n here      
-var n = 5;
-generateNumberTriangle(n);
-
-function generateNumberTriangle(n) {
-    var width = (2 * n) -1; // Always the case.
-    var midpoint = Math.floor(width / 2); // Middle of pyramid.
-    let level = ''; // will be reset each level loop
-
-    for(var i = 0; i < n; i++) { // Looping through levels
-        level = '';
-        for(var j = 0; j < width; j++) {
-            if(j < midpoint-i || j > midpoint+i) {
-                level += '.';
-            } else {
-                level += '#';
-            }
-        }
-        console.log(level);
+function generatePyramid() {
+  var totalNumberofRows = 5;
+  var arr = new Array();
+  for (var i = 1; i <= totalNumberofRows; i++) {
+    for (var j = 1; j <= i; j++) {
+      arr.push(j);
+      console.log(j);
     }
+    console.log("\n");
+  }
 }
